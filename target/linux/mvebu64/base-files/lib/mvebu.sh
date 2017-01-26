168ddfc67fe62f6b924950c5aa8a519f77f5b885
Vladimir Vid <vladimir.vid@sartura.hr>
2017-01-26 18:33:53 +0100

    target: add support for mvebu64 target
    Signed-off-by: Vladimir Vid <vladimir.vid@sartura.hr>

---
+++ target/linux/mvebu64/base-files/lib/mvebu.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Copyright (C) 2017 OpenWrt.org
+#
+
+MVEBU_BOARD_NAME=
+MVEBU_MODEL=
+
+mvebu_board_detect() {
+	local machine
+	local name
+
+	machine=$(cat /proc/device-tree/model)
+
+	case "$machine" in
+	*)
+		name="unknown"
+		;;
+	esac
+
+	[ -z "$name" ] && name="unknown"
+
+	[ -z "$MVEBU_BOARD_NAME" ] && MVEBU_BOARD_NAME="$name"
+	[ -z "$MVEBU_MODEL" ] && MVEBU_MODEL="$machine"
+
+	[ -e "/tmp/sysinfo/" ] || mkdir -p "/tmp/sysinfo/"
+
+	echo "$MVEBU_BOARD_NAME" > /tmp/sysinfo/board_name
+	echo "$MVEBU_MODEL" > /tmp/sysinfo/model
+}
+
+mvebu_board_name() {
+	local name
+
+	[ -f /tmp/sysinfo/board_name ] || mvebu_board_detect
+	[ -f /tmp/sysinfo/board_name ] && name=$(cat /tmp/sysinfo/board_name)
+	[ -z "$name" ] && name="unknown"
+
+	echo "$name"
+}

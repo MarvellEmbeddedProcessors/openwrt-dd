769328690f4cc68932cdedc39fd24abfe0deb71a
Vladimir Vid <vladimir.vid@sartura.hr>
2017-03-31 17:35:45 +0200

    mvebu64: add support for Armada Community Board (ESPRESSObin)
    Signed-off-by: Vladimir Vid <vladimir.vid@sartura.hr>

--- target/linux/mvebu64/base-files/lib/mvebu.sh
+++ target/linux/mvebu64/base-files/lib/mvebu.sh
@@ -22,6 +22,9 @@ mvebu_board_detect() {
 	*"Marvell Armada 3720 Development Board DB-88F3720-DDR3")
 		name="armada-3720-db"
 		;;
+	*"Marvell Armada 3700 Community Board")
+		name="armada-ESPRESSObin"
+		;;
 	*)
 		name="unknown"
 		;;

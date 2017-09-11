7e6ced4e689384b74c88a0a420816a977da41f59
Damir Samardzic <damir.samardzic@sartura.hr>
2017-09-11 14:09:27 +0200

    mvebu38x: added swconfig to solidrun profile

--- target/linux/mvebu38x/profiles/solidrun.mk
+++ target/linux/mvebu38x/profiles/solidrun.mk
@@ -7,7 +7,7 @@
 
 define Profile/Solidrun-Clearfog-A1
   NAME:=SolidRun ClearFog A1 board
-  PACKAGES:= uboot-mvebu38x-clearfog
+  PACKAGES:= uboot-mvebu38x-clearfog swconfig
 endef
 
 define Profile/Solidrun-Clearfog-A1/Description
